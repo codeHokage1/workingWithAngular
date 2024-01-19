f3bea924deeafb69b2cb239b8872f9908c96fc07
Sodiq Farhan <sodiqfarhan360@gmail.com>
2024-01-19 14:40:31 +0100

    Implement and add navigation

--- online-store/src/app/app.component.ts
+++ online-store/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { FirstCompComponent } from './first-comp/first-comp.component';
 import { AboutComponent } from './about/about.component';
 import { MenuComponent } from './menu/menu.component';
@@ -8,9 +8,17 @@ import { MenuComponent } from './menu/menu.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FirstCompComponent, AboutComponent, MenuComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FirstCompComponent,
+    AboutComponent,
+    MenuComponent,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'online-store';

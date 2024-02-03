1b36668ae02644d4f57f477f78ef9f3b21abee01
Sodiq Farhan <sodiqfarhan360@gmail.com>
2024-02-03 11:02:21 +0100

    Implement a toast for the notification

--- netflix-clone/src/app/services/login.service.spec.ts
+++ netflix-clone/src/app/services/login.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { LoginService } from './login.service';
 
+
 describe('LoginService', () => {
   let service: LoginService;
 
--- netflix-clone/src/app/services/login.service.ts
+++ netflix-clone/src/app/services/login.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private toast: ToastrService) { }
 
   loginUser(email: string, password: string) {
     if(!email || !password) {
-      alert("Please enter a valid email and password");
+      // alert("Please enter a valid email and password");
+      this.toast.error("Please enter a valid email and password");
       return;
     }
 
     else{
-      alert("Login successful!");
+      // alert("Login successful!");
+      this.toast.success("Login successful!");
       localStorage.setItem("token", Math.floor(Math.random() * 10000) + "")
       this.router.navigateByUrl('/browse');
     }
